e4d8c2a1f186d50f37b3e1de0cb867914da126e6
Dennis Federico <dennisfederico@gmail.com>
2022-11-27 22:32:36 +0100

    Initial commit

---
+++ Assets/Scripts/CrawlerWithWall.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrawlerWithWall : MazeGenerator {
+    public override void GenerateMap() {
+        var minHeight = mazeSize.height / 6;
+        SingleHorizontalCrawl(Random.Range(minHeight, mazeSize.height - minHeight));
+        SingleHorizontalCrawl(Random.Range(minHeight, mazeSize.height - minHeight));
+
+        var minWidth = mazeSize.width / 6;
+        SingleVerticalCrawl(Random.Range(minWidth, mazeSize.width - minWidth));
+        SingleVerticalCrawl(Random.Range(minWidth, mazeSize.width - minWidth));
+        SingleVerticalCrawl(Random.Range(minWidth, mazeSize.width - minWidth));
+    }
+
+    private void SingleHorizontalCrawl(int startZ) {
+        bool done = false;
+        int x = 1;
+        int z = startZ;
+
+        while (!done) {
+            map[x, z] = 0;
+            if (Random.Range(0, 100) < 40) {
+                x += Random.Range(0, 2);
+            } else {
+                z += Random.Range(-1, 2);
+            }
+
+            done |= (x < 1 || x >= mazeSize.width - 1 || z < 1 || z >= mazeSize.height - 1);
+        }
+    }
+
+    private void SingleVerticalCrawl(int startX) {
+        bool done = false;
+        int x = startX;
+        int z = 1;
+
+        while (!done) {
+            map[x, z] = 0;
+            if (Random.Range(0, 100) < 40) {
+                z += Random.Range(0, 2);
+            } else {
+                x += Random.Range(-1, 2);
+            }
+
+            done |= (x < 1 || x >= mazeSize.width - 1 || z < 1 || z >= mazeSize.height - 1);
+        }
+    }
+}
